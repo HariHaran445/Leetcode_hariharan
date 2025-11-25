378b8d17f02e2269b79e8642afed46ae9f7a6bdc
HariHaran T <hariharant04nirmala@gmail.com>
2025-11-25 21:44:10 +0530

    Time: 2 ms (37.88%), Space: 44.2 MB (8.61%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String addBinary(String a, String b) {
+        StringBuffer sb=new StringBuffer();
+       int i=a.length()-1;;
+       int j=b.length()-1;;
+       int carry=0;
+       while(i>=0 ||j>=0 ||carry==1){
+        int bita= (i>=0) ? a.charAt(i) - '0': 0;
+        int bitb= (j>=0) ? b.charAt(j) - '0': 0;
+
+        int sum=bita+bitb+carry;
+        sb.insert(0,sum%2);
+        carry=sum/2;
+        i--;
+        j--;
+       }
+       return sb.toString();
+    }
+}
