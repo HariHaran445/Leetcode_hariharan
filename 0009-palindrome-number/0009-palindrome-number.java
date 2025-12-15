84b4c320e2e17b220f5b02dd22107ac21308e4fd
HariHaran T <hariharant04nirmala@gmail.com>
2025-12-15 21:18:34 +0530

    Time: 5 ms (73.72%), Space: 46 MB (18.75%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -4,7 +4,7 @@ class Solution {
         int sum=0;
         if(x<0){
             return false;
-        }
+        }//
         while(x!=0){
             int digit=x%10;
             sum=sum*10+digit;
