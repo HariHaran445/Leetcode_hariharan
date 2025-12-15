2ddc76b550c6529ffbc954fe4d37540d27f968d3
HariHaran T <hariharant04nirmala@gmail.com>
2025-12-15 21:18:15 +0530

    Time: 4 ms (99.98%), Space: 45.8 MB (60.29%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,25 @@
+class Solution {
+    public static boolean isPalindrome(int x) {
+        int n=x;
+        int sum=0;
+        if(x<0){
+            return false;
+        }
+        while(x!=0){
+            int digit=x%10;
+            sum=sum*10+digit;
+            x=x/10;
+        }
+    if(sum==n){
+        return true;
+    }
+    else{
+        return false;
+    }
+    }
+    public static void main(String []args){
+        Scanner s=new Scanner(System.in);
+        int a= s.nextInt();
+        System.out.print(isPalindrome(a));
+    }
+}
