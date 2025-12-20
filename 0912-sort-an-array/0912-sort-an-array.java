f5b71894f14a6bd208e4ed59c03382037e83c1e9
HariHaran T <hariharant04nirmala@gmail.com>
2025-12-20 18:48:30 +0530

    Time: 39 ms (28.79%), Space: 86.9 MB (7.99%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.java
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -1,6 +1,7 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-             int n=nums.length;
+
+        /*     int n=nums.length;
        mergesort(nums,0,n-1); 
        return nums;
     }
@@ -29,5 +30,52 @@ class Solution {
         }
         while (i < n1) arr[k++] = L[i++];
         while (j < n2) arr[k++] = R[j++];
+    }
+}
+*/
+
+int n = nums.length;
+
+        // Phase 1: Build Max-Heap (rearrange array)
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            heapify(nums, n, i);
+        }
+
+        // Phase 2: One by one extract elements from heap
+        for (int i = n - 1; i > 0; i--) {
+            // Move current root (largest) to end
+            int temp = nums[0];
+            nums[0] = nums[i];
+            nums[i] = temp;
+
+            // call max heapify on the reduced heap
+            heapify(nums, i, 0);
+        }
+        return nums;
+    }
+
+    // To heapify a subtree rooted with node i which is an index in arr[]
+    void heapify(int arr[], int n, int i) {
+        int largest = i; // Initialize largest as root
+        int l = 2 * i + 1; // left = 2*i + 1
+        int r = 2 * i + 2; // right = 2*i + 2
+
+        // If left child is larger than root
+        if (l < n && arr[l] > arr[largest])
+            largest = l;
+
+        // If right child is larger than largest so far
+        if (r < n && arr[r] > arr[largest])
+            largest = r;
+
+        // If largest is not root
+        if (largest != i) {
+            int swap = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = swap;
+
+            // Recursively heapify the affected sub-tree
+            heapify(arr, n, largest);
+        }
     }
 }
